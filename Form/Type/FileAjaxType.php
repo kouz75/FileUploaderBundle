379903f28098e5ef91c14dfbdfda08b564403655
kouz75 <kouz2@free.fr>
2017-04-19 16:55:04 +0200

    Update FileAjaxType.php

--- Form/Type/FileAjaxType.php
+++ Form/Type/FileAjaxType.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Copyright 2014 Jonathan Bouzekri. All rights reserved.
  *
@@ -7,15 +6,13 @@
  * @license https://github.com/jbouzekri/FileUploaderBundle/blob/master/LICENSE
  * @link https://github.com/jbouzekri/FileUploaderBundle
  */
-
 namespace Jb\Bundle\FileUploaderBundle\Form\Type;
-
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Jb\Bundle\FileUploaderBundle\Service\FileHistoryManagerInterface;
-
 /**
  * FileAjaxType
  */
@@ -29,7 +26,7 @@ class FileAjaxType extends AbstractType
     /**
      * Constructor
      *
-     * @param \Jb\Bundle\FileUploaderBundle\Form\Type\FileHistoryManagerInterface $fileHistoryManager
+     * @param FileHistoryManagerInterface $fileHistoryManager
      */
     public function __construct(FileHistoryManagerInterface $fileHistoryManager)
     {
@@ -39,23 +36,25 @@ class FileAjaxType extends AbstractType
     /**
      * {@inheritDoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         // Endpoint mandatory for fileupload bundle
         $resolver->setRequired(array('endpoint'));
 
-        $resolver->setOptional(array(
+        $resolver->setDefined(array(
             'download_link',
             'remove_link',
             'loading_file',
-            'resolver_key'
+            'resolver_key',
+            'progress'
         ));
 
         $resolver->setDefaults(array(
             'download_link' => true,
             'remove_link' => true,
             'loading_file' => 'bundles/jbfileuploader/img/ajax-loader-small.gif',
-            'resolver_key' => 'upload_resolver'
+            'resolver_key' => 'upload_resolver',
+            'progress'=>false
         ));
     }
 
@@ -84,6 +83,7 @@ class FileAjaxType extends AbstractType
         $view->vars['download_link'] = $options['download_link'];
         $view->vars['remove_link'] = $options['remove_link'];
         $view->vars['loading_file'] = $options['loading_file'];
+        $view->vars['progress'] = $options['progress'];
         $view->vars['use_crop'] = false;
     }
 
@@ -92,14 +92,6 @@ class FileAjaxType extends AbstractType
      */
     public function getParent()
     {
-        return 'text';
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getName()
-    {
-        return 'jb_file_ajax';
+        return TextType::class;
     }
 }
